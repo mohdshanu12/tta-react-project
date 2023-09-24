b5f0486204ece49ad694dc2e56f23cea3e5bb3c1
mohdshanu12 <shanumohd524@gmail.com>
2023-09-24 16:01:52 +0530

    success

--- src/App.js
+++ src/App.js
@@ -1,70 +1,35 @@
 //import logo from './logo.svg';
-//import './App.css';
+import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 function App() {
   return (
      <>
-     <h3 className='text-left text-danger text-capitalize my-3'>LOREM IPSUM 
-      
-     <div className="col text-right">
-      <a href="https://example.com" className="text-lg">Link1</a>
-    </div> 
-</h3>
-          
-     
-     <div className="container">
-  <div className="row">
-    <div className="col-sm">
-      <div className="card"  >
-  <img src="https://picsum.photos/200/301" className="card-img-top" alt="..."
-  height="200px"/>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+      <div className='section layout bg-light'>
+      <div className='container'>
+        <div className='row head'>
+            <div className='col-md-3 colHeadLorem'>
+                <h3>LOREM IPSUM</h3>
+            </div>
+            <div className='col-md-9 colHeadLink'>
+                <a href='' className='link'>LINK1</a>
+            </div> 
+        </div>
+
+        <div className='row center'>
+            <div className='col-md-6 colCenter'>
+                <h3 className='centerHead'>THIS IS SOME CONTENT ALIGNED IN THS CENTER<br/> VERTICALLY AND HORIZONTALLY</h3>
+            </div>
+            <div className='col-md-6 colCenter'></div>
+        </div>
+
+        <div className='row footer'>
+            <div className='col-md-12 colFooter'>
+                <h3>THIS IS THE FOOTER CONTENT WHICH IS ALIGNED<br/>
+                <span className='left'>LEFT</span></h3>
+            </div>
+        </div>
+      </div>
     </div>
-    <div className="col-sm">
-      <div className="card"  >
-  <img src="https://picsum.photos/200/302" className="card-img-top" alt="..."
-  height="200px"/>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-    </div>
-    <div className="col-sm">
-      <div className="card"  >
-  <img src="https://picsum.photos/202/300" className="card-img-top" alt="..."
-  height="200px"/>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-    </div>
-  </div>
-</div>
-
- 
-
-{/* Second method for cintent horizontally- vertically */}
-
-<div className="d-flex align-items-center justify-content-center my-3">
-    <div className="p-2 m-2 bg-info text-white shadow rounded-2">THIS IS SOME CONTENTENT ALIGNED IN THE CENTER VERTCALLY AND HORIZONTALLY</div>
-     
-</div>
-
-{/* footer content */}
-
-<footer class="footer text-left">
-  <div class="container">
-  @ This is a contentent which is alignment left  </div>
-</footer>
      </>
   );
 }
